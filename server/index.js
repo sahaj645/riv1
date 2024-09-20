733bd02d19bd06d4301098859cd6fd45f86f1a22
Sahil Murhekar <131905293+sahilmurhekar@users.noreply.github.com>
2024-09-21 03:28:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,13 +8,19 @@ mongoose.connect('mongodb+srv://gravitastam2024:Sahil%40tam123@cluster0.inn1c.mo
     .then(() => console.log("DB Connected"))
     .catch(err => console.log("DB not connected", err));
 
-// Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://tam-gravitas-vit.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: ['https://tam-gravitas-vit.vercel.app'],
-    methods: ['POST','GET']
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: ['https://tam-gravitas-vit.vercel.app'],
+//     methods: ['POST','GET']
+// }));
 
 // Routes
 app.use('/', require('./routes/authroutes'));
